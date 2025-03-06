142f5f026e4bc9843ec44e497d4000d9343bc946
Chendage0113 <j1136746106@Gmail.com>
2025-03-06 20:21:01 +0800

    Delete api directory

--- api/index.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    const { lat, long } = req.query;
-    if (!lat || !long) {
-        return res.status(400).json({ error: "缺少纬度或经度参数" });
-    }
-    const data = `纬度: ${lat}, 经度: ${long}`;
-    res.status(200).send(data);
-}
